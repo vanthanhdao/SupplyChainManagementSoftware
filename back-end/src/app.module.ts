5b79aa24ed31d60b7abde86585e075205ba22d68
vanthanhdao <daothanh1411@gmail.com>
2024-10-06 10:45:00 +0700

    update libaries and change table from user to users

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
+import { Users } from './users/entities/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
