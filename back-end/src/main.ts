2796c8f7cf825b3f516d214906d37bd318c9e741
vanthanhdao <daothanh1411@gmail.com>
2024-10-06 10:48:42 +0700

    set global '.../api/v1' and set global validationpipe

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 }
 bootstrap();
